c2d1030ff358caa898642263d462121421940347
Meagan Palmaira <mmegcode@gmail.com>
2022-08-05 11:50:17 -0400

    added routes

--- src/App.js
+++ src/App.js
@@ -5,52 +5,23 @@ import {
   Route,
   Link
 } from "react-router-dom";
-import {CoinList, CoinPage, Portfolio} from 'pages'
+import {HomePage, CoinPage, Portfolio} from 'pages'
 
 export default function App() {
   return (
     <Router>
       <div>
         <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
         </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
+          <Route exact path="/" component={HomePage}/>
+          <Route exact path="/portfolio" component={Portfolio}/>
+          <Route exact path='/coins/:coinId' component={CoinPage}/>
+git
         </Switch>
       </div>
     </Router>
   );
 }
 
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
-}
