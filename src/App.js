54848e4637904b067a1c0fc1cb6caf7a689aee4c
Meagan Palmaira <mmegcode@gmail.com>
2022-08-06 00:37:08 -0400

    fixed changes to import from pages

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {
   Route,
   Link
 } from "react-router-dom";
-import { HomePage, CoinPage, Portfolio } from 'pages'
+import { HomePage, CoinPage, Portfolio } from './pages'
 
 export default function App() {
   this.state = {
@@ -21,7 +21,6 @@ export default function App() {
           <Route exact path="/" component={HomePage}/>
           <Route exact path="/portfolio" component={Portfolio}/>
           <Route exact path='/coinpage/:coinId' component={CoinPage}/>
-git
         </Switch>
       </div>
     </Router>
---
+++ src/components/Theme/Theme.js
@@ -0,0 +1,11 @@
+const darkTheme = {
+    bodyColor: "#1F2128",
+    cardColor: '#191B1F',
+    fontColor: '#FFFFFF',
+}
+
+const lightTheme = {
+    bodyColor: "#FCFCFC",
+    cardColor: '#FFFFFF',
+    fontColor: '#2C2F36',
+}
