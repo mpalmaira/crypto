95b7c03a06f2640ff021f41664a5460d0e7be307
Meagan Palmaira <mmegcode@gmail.com>
2022-08-08 10:44:39 -0400

    added coinItem

--- src/components/CoinItem/CoinItem.js
+++ src/components/CoinItem/CoinItem.js
@@ -8,7 +8,8 @@ const CoinItem = (props) => {
             <div className='img-symbol'>
                 <img src ={props.coins.image} alt = ''/>
             </div>
-            
+            <p>{props.coins.name}</p>
+            <p>({props.coins.symbol})</p>
             <p>${props.coins.current_price}</p>
             <p>${props.coins.current_price}</p>
             <p>{props.coins.price_change_percentage_1h_in_currency}%</p>
