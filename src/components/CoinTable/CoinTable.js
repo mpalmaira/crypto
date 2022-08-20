12234ce03fdc25e6031dcf2a04852a9b11b3fef3
Meagan Palmaira <mmegcode@gmail.com>
2022-08-20 17:13:17 -0400

    added infinite scroll to coin table

--- src/components/CoinTable/CoinTable.js
+++ src/components/CoinTable/CoinTable.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import InfiniteScroll from "react-infinite-scroll-component";
 import {
   TableContainer,
   StyledTable,
@@ -11,17 +12,21 @@ import CoinItem from "../CoinItem/CoinItem";
 class CoinTable extends React.Component {
   state = {
     isLoading: false,
-    coins: null,
+    coins: [],
     hasError: false,
+    page: 1,
   };
 
   getData = async () => {
     try {
       this.setState({ isLoading: true });
       const { data } = await axios(
-        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${this.props.currency}&order=market_cap_desc&per_page=50&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d`
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${this.props.currency}&order=market_cap_desc&per_page=10&page=${this.state.page}&sparkline=true&price_change_percentage=1h%2C24h%2C7d`
       );
-      this.setState({ coins: data, isLoading: false });
+      this.setState({
+        coins: [...this.state.coins, ...data],
+        isLoading: false,
+      });
     } catch (err) {
       this.setState({ hasError: true, isLoading: false });
     }
@@ -29,29 +34,49 @@ class CoinTable extends React.Component {
   componentDidMount() {
     this.getData();
   }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.page !== this.state.page) {
+      this.getData();
+    }
+  }
   render() {
     return (
       <TableContainer>
         {this.state.coins && (
           <StyledTable>
-            <thead>
-              <tr>
-                <TableHeadRank>#</TableHeadRank>
-                <TableHeadName>Name</TableHeadName>
-                <TableHead>Price</TableHead>
-                <TableHead>1h%</TableHead>
-                <TableHead>24h%</TableHead>
-                <TableHead>7d%</TableHead>
-                <th>24h Volume/Market Cap</th>
-                <th>Circulating/Total Supply</th>
-                <th>Last 7d</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.state.coins.map((coins) => {
-                return <CoinItem key={coins.id} coins={coins} symbol = {this.props.symbol} />;
-              })}
-            </tbody>
+            <InfiniteScroll
+              dataLength={this.state.coins.length}
+              next={() => {
+                this.setState({ page: this.state.page + 1 });
+              }}
+              hasMore={true}
+              loader={<h4>Loading 10 more items...</h4>}
+            >
+              <thead>
+                <tr>
+                  <TableHeadRank>#</TableHeadRank>
+                  <TableHeadName>Name</TableHeadName>
+                  <TableHead>Price</TableHead>
+                  <TableHead>1h%</TableHead>
+                  <TableHead>24h%</TableHead>
+                  <TableHead>7d%</TableHead>
+                  <th>24h Volume/Market Cap</th>
+                  <th>Circulating/Total Supply</th>
+                  <th>Last 7d</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.coins.map((coins) => {
+                  return (
+                    <CoinItem
+                      key={coins.id}
+                      coins={coins}
+                      symbol={this.props.symbol}
+                    />
+                  );
+                })}
+              </tbody>
+            </InfiniteScroll>
           </StyledTable>
         )}
       </TableContainer>
